3f1b84b939a123042fcea2ec4dec780c04f01a7a
LJ <1789831043@qq.com>
2020-02-27 22:30:14 +0800

    提交一次代码

---
+++ 8-9.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+class BaseClass {
+public:
+	virtual ~BaseClass() {
+		cout << "调用基类析构函数" << endl;
+	}
+	void fn1() {};
+};
+
+class DerivedClass :public BaseClass {
+public:
+	~DerivedClass(){
+		cout << "调用派生类析构函数" << endl;
+	}
+	void fn1() {};
+};
+
+int main()
+{
+	BaseClass* sp = new DerivedClass;          //动态生成对象
+	delete sp;                         //利用基类指针的虚析构函数，派生类的析构函数被调用，内存空间被正确释放了
+	return 0;
+}
+
