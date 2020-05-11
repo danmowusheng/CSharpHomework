cd733fbe15eebe3db5301d6287600bd8743cac90
smalljie <1789831043@qq.com>
2020-05-11 21:43:28 +0800

    第十周作业

---
+++ Homework10/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CrawlerPlus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.labstatus = new System.Windows.Forms.Label();
+            this.dgvResult = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labstatus);
+            this.panel1.Controls.Add(this.btn_start);
+            this.panel1.Controls.Add(this.txtURL);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(799, 88);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(23, 25);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(446, 25);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.Text = "http://www.cnblogs.com/dstang2000/";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(529, 24);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(93, 26);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.Text = "开始";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // labstatus
+            // 
+            this.labstatus.AutoSize = true;
+            this.labstatus.Location = new System.Drawing.Point(29, 65);
+            this.labstatus.Name = "labstatus";
+            this.labstatus.Size = new System.Drawing.Size(0, 15);
+            this.labstatus.TabIndex = 2;
+            // 
+            // dgvResult
+            // 
+            this.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResult.Location = new System.Drawing.Point(24, 94);
+            this.dgvResult.Name = "dgvResult";
+            this.dgvResult.RowHeadersWidth = 51;
+            this.dgvResult.RowTemplate.Height = 27;
+            this.dgvResult.Size = new System.Drawing.Size(615, 481);
+            this.dgvResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 587);
+            this.Controls.Add(this.dgvResult);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labstatus;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.DataGridView dgvResult;
+    }
+}
+
---
+++ Homework10/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace CrawlerPlus
+{
+    public partial class Form1 : Form
+    {
+        DateTime startTime;
+        BindingSource resultBindingSource = new BindingSource();
+        Crawler crawler = new Crawler();
+        Thread thread = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dgvResult.DataSource = resultBindingSource;
+
+            //委托事件
+            crawler.PageDownloaded += Crawler_PageDownloaded;
+            crawler.CrawlerStopped += Crawler_CrawlerStopped;
+        }
+
+        private void Crawler_CrawlerStopped(Crawler obj)
+        {
+            DateTime endTime = DateTime.Now;
+            Action action = () => labstatus.Text = "爬虫已停止,用时：" + (endTime - startTime);
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void Crawler_PageDownloaded(Crawler crawler, int index, string url, string info)
+        {
+            var pageInfo = new { Index = index, URL = url, Status = info };
+            Action action = () => { resultBindingSource.Add(pageInfo); };
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            startTime = DateTime.Now;
+            resultBindingSource.Clear();
+            crawler.StartURL = txtURL.Text;
+
+            Match match = Regex.Match(crawler.StartURL, Crawler.urlParseRegex);
+            if (match.Length == 0) return;
+            string host = match.Groups["host"].Value;
+            crawler.HostFilter = "^" + host + "$";
+            crawler.FileFilter = ".html?$";
+
+            if (thread != null)
+            {
+                thread.Abort();
+            }
+            thread = new Thread(crawler.Start);
+            thread.Start();
+            labstatus.Text = "爬虫已启动....";
+        }
+    }
+}
