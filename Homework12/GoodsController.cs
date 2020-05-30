b77a303af729f61a1924cefad668ffb4ae3c5785
smalljie <1789831043@qq.com>
2020-05-30 18:50:49 +0800

    这是第十二周作业

---
+++ Homework12/GoodsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderWeb.Model;
+
+namespace OrderWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoodsController : ControllerBase
+    {
+        private readonly OrderContext orderDB;
+
+        public GoodsController(OrderContext context)
+        {
+            orderDB = context;
+        }
+
+
+        // GET: api/Goods/5
+        [HttpGet("{id}")]
+        public ActionResult<List<Goods>> GetAllGoods()
+        {
+            IQueryable<Goods> query = orderDB.Goods;
+
+            return query.ToList();
+        }
+
+        // GET: api/Items/5
+        //依据id查询商品
+        [HttpGet("{id}")]
+        public ActionResult<Goods> GetGoodsByID(string id)
+        {
+            Goods goods;
+            IQueryable<Goods> query = orderDB.Goods;
+            query = query.Where(t => t.ID == id);
+            goods = query.FirstOrDefault();
+            return goods;
+        }
+
+        //依据价格查询商品,小于等于均可
+        [HttpGet("{id}")]
+        public ActionResult<List<Goods>> GetItemByOrderID(int  price)
+        {
+            IQueryable<Goods> query = orderDB.Goods;
+            query = query.Where(t => t.Price<=price);
+
+            return query.ToList();
+        }
+
+        // POST: api/Orders
+        //添加新商品
+        [HttpPost]
+        public ActionResult<Goods> PostItem(Goods goods)
+        {
+
+            try
+            {
+                orderDB.Goods.Add(goods);
+                orderDB.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                //返回错误信息
+                return BadRequest(e.InnerException);
+            }
+
+            return goods;
+        }
+
+        // PUT: api/Orders/5
+        //修改商品信息
+        [HttpPut("{id}")]
+        public ActionResult<Item> PutItem(string id, Goods goods,double? price,string? name)
+        {
+            if (id != goods.ID)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                orderDB.Entry(goods).State = EntityState.Modified;
+                goods.Name = name;
+                goods.Price = (double)price;
+                orderDB.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                //返回错误信息
+                return BadRequest(e.InnerException);
+            }
+
+            return NoContent();
+        }
+
+        //删除订单
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(string id)
+        {
+            try
+            {
+                var goods = orderDB.Goods.FirstOrDefault(t => t.ID == id);
+                if (goods != null)
+                {
+                    orderDB.Remove(goods);
+                    orderDB.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                //返回错误信息
+                return BadRequest(e.InnerException);
+            }
+
+            return NoContent();
+        }
+
+        private bool GoodsExists(string id)
+        {
+            return orderDB.Goods.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Homework12/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using OrderWeb.Model;
+
+namespace OrderWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<OrderContext>(options =>
+               options.UseMySql(Configuration.GetConnectionString("OrderDatabase"))
+               //这一段控制语句暂时不写
+              // mysqloption=>mysqloption.serverVersion(new Version(5,7,30),servertype.mysql
+              );
+            services.AddControllers();      
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
